a98c4cd56a97daf6336066047ef29112683f60cb
sezer demir dedek <sezerddedek@gmail.com>
2024-05-19 01:33:40 +0300

    First folder layout added.

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import ProductCard from "../components/ProductCard";
+
+const Login = () => {
+	return (
+		<section className="w-full h-full flex flex-row justify-center items-center">
+			Login
+			<ProductCard />
+		</section>
+	);
+};
+
+export default Login;
